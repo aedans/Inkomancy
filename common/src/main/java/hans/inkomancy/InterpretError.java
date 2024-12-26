c30da04ff67328dc339083394e0ae19c2af3ebd9
Aedan Smith <aedan.smith@outlook.com>
2024-12-25 22:06:25 -0800

    Remove

--- common/src/main/java/hans/inkomancy/InterpretError.java
+++ common/src/main/java/hans/inkomancy/InterpretError.java
@@ -1,17 +1,11 @@
 package hans.inkomancy;
 
-import java.util.Collection;
-
 public abstract class InterpretError extends Exception {
   private InterpretError(String message) {
     super(message);
   }
 
   public static class Conversion extends InterpretError {
-    public Conversion(Morpheme morpheme, Collection<Morpheme.Type> types) {
-      super("cannot convert " + morpheme + " to " + types.stream().map(x -> x.toString().toLowerCase()).toList());
-    }
-
     public Conversion(Morpheme morpheme, Morpheme.Type type) {
       super("cannot convert " + morpheme + " to " + type);
     }
